7ad3055c8dc4d30e6fc07c644887c2d117faa2b0
zhuwenqin <zhuwenqin_vendor@sensetime.com>
2020-04-20 18:48:22 +0800

    update

---
+++ guard/pages/device.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# @Time: 2020/4/20 17:59
+# @Author: wenqin_zhu
+# @File: device.py
+# @Software: PyCharm
+
+import time
+from selenium.webdriver.common.by import By
+from guard.pages.classes.basepage import BasePage
+from guard.pages.components.group_tree import GroupTree
+
+
+class DevicePage(BasePage):
+
+    def add_device_and_type_is_camera(self):
+        """ 添加设备类型为 摄像头 的设备 """
+        pass
---
+++ guard/pages/task.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# @Time: 2020/4/20 17:59
+# @Author: wenqin_zhu
+# @File: task.py
+# @Software: PyCharm
+
+import time
+from selenium.webdriver.common.by import By
+from guard.pages.classes.basepage import BasePage
+from guard.pages.components.group_tree import GroupTree
+
+
+class TaskPage(BasePage):
+
+    def add_task_to_parked_vehicle(self):
+        """ 添加车辆违停任务 """
+        pass
